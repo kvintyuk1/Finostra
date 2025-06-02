59a31fa27ffb8c0d062c4d0b93c2391eaf756c63
nazar-kryvychko <110460905+Kriv09@users.noreply.github.com>
2025-06-02 11:32:54 +0300

    Xyuna

--- finostrafrontend/src/redux/slices/transactionSlice.js
+++ finostrafrontend/src/redux/slices/transactionSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
+import axiosInstance from "../../utils/axiosInstance";
 
 const URL = 'http://localhost:8081/api/v1';
 
